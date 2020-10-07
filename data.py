62f282f69202789e921581d3ea0074576b8445a3
antoine planchon <antoine.planchon@hec.ca>
2020-10-07 18:39:57 +0200

    progress

---
+++ data.py
@@ -0,0 +1,149 @@
+import requests
+import pandas as pd
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv('GLASSNODES_API_KEY')
+
+def get_btc_OHLC():
+    url_glassnode = 'https://api.glassnode.com/v1/metrics/market/price_usd_ohlc'
+    parameters = {
+     'api_key' : API_KEY,
+     'a' : 'BTC'
+             }
+    data = requests.get(url  = url_glassnode, params = parameters).json()
+    close = []
+    high = []
+    low = []
+    Open = []
+    date = []
+    for x in range(len(data)) :
+        close.append(data[x]['o']['c'])
+        high.append(data[x]['o']['h'])
+        low.append(data[x]['o']['l'])
+        Open.append(data[x]['o']['o'])
+        date.append(data[x]['t'])
+    dict_df = {
+        'date' : date,
+        'close' : close,
+        'open' : Open,
+        'high' : high,
+        'low' : low
+    }
+    df = pd.DataFrame(dict_df)
+    df['date'] = pd.to_datetime(df['date'], unit = 's')
+    df.set_index('date', inplace = True)
+    return df
+
+def get_rsi(df,n):
+    n = 14
+    df_rsi = df.copy()
+    def rma(x, n, y0):
+        a = (n-1) / n
+        ak = a**np.arange(len(x)-1, -1, -1)
+        return np.append(y0, np.cumsum(ak * x) / ak / n + y0 * a**np.arange(1, len(x)+1))
+    df_rsi.reset_index(inplace = True)
+    df_rsi['change'] = df_rsi['close'].diff()
+    df_rsi['gain'] = df_rsi.change.mask(df_rsi.change < 0, 0.0)
+    df_rsi['loss'] = -df_rsi.change.mask(df_rsi.change > 0, -0.0)
+    df_rsi.loc[n:,'avg_gain'] = rma( df_rsi.gain[n+1:].values, n, df_rsi.loc[:n, 'gain'].mean())
+    df_rsi.loc[n:,'avg_loss'] = rma( df_rsi.loss[n+1:].values, n, df_rsi.loc[:n, 'loss'].mean())
+    df_rsi['rs'] = df_rsi.avg_gain / df_rsi.avg_loss
+    df_rsi['rsi_14'] = 100 - (100 / (1 + df_rsi.rs))
+    df_rsi.set_index('date', inplace = True)
+    return df_rsi['rsi_14']
+
+
+def get_MA(df):
+    ma_5 = df.iloc[:,0].rolling(window=5).mean()
+    ma_10 = df.iloc[:,0].rolling(window=10).mean()
+    ma_25 = df.iloc[:,0].rolling(window=25).mean()
+    ma_50 = df.iloc[:,0].rolling(window=50).mean()
+    ma_100 = df.iloc[:,0].rolling(window=100).mean()
+    ma_200 = df.iloc[:,0].rolling(window=200).mean()
+    return ma_5, ma_10, ma_25, ma_50, ma_100, ma_200
+
+
+def get_EMA(df):
+    ema_20 = df.iloc[:,0].ewm(span = 20).mean()
+    ema_30 = df.iloc[:,0].ewm(span = 30).mean()
+    return ema_20, ema_30
+
+
+def get_SMI(df) :
+    period = 10
+    df_smi = df.copy()
+    Max= df.iloc[:,0].rolling(10).max()
+    Min = df.iloc[:,0].rolling(10).min()
+    Mean = (Min + Max )/2
+    h = df_smi['close'] - Mean
+    HS2 = h.ewm(span = 3).mean().ewm(span = 3).mean()
+    DHL2  = (Max -Min).ewm(span = 3).mean().ewm(span = 3).mean()
+    return HS2 / DHL2
+
+def get_CCI(df)  :
+    df_cci = df.copy()
+    cours_moyen = (df['close'] + df['high'] + df['low']) / 3
+    SMATP = cours_moyen.rolling(window=20).mean()
+    deviation_moyenne  = abs(SMATP - cours_moyen).rolling(window=20).mean()
+    CCI = (cours_moyen - SMATP) / (0.015*deviation_moyenne)
+    return CCI
+
+
+def get_william_A_D(df) :
+    df_a_d = df.copy()
+    TRH = {}
+    for x in range(1,len(df_a_d)) :
+        if df_a_d['close'][x-1] > df_a_d['high'][x] :
+            TRH[df_a_d.index[x]] = df_a_d['close'][x-1]
+        else :
+            TRH[df_a_d.index[x]] = df_a_d['high'][x]
+    TRL = {}
+    for x in range(1,len(df)) :
+        if df_a_d['close'][x-1] < df_a_d['low'][x] :
+            TRL[df_a_d.index[x]] = df_a_d['close'][x-1]
+        else :
+            TRL[df_a_d.index[x]] = df_a_d['low'][x]
+    A_D = {}
+    for x in range(len(df_a_d)):
+        if x == 0 :
+            A_D[df_a_d.index[x]] = 0
+        elif df_a_d['close'][x] > df_a_d['close'][x-1] :
+            A_D[df_a_d.index[x]] = df_a_d['close'][x] - TRL[df_a_d.index[x]]
+        elif df_a_d['close'][x] < df_a_d['close'][x-1] :
+            A_D[df_a_d.index[x]] = df_a_d['close'][x] - TRH[df_a_d.index[x]]
+        else :
+            A_D[df_a_d.index[x]] = 0
+    df_a_d['A_D'] = A_D.values()
+    df_a_d['williams_A_D'] = df_a_d['A_D'].cumsum()
+    return df_a_d['williams_A_D']
+
+
+def get_ATR(df):
+    df_atr = df.copy()
+    a = df_atr['high'] - df_atr['low']
+    b = abs(df_atr['high'] - df_atr['close'])
+    c = abs(df_atr['low'] -df_atr['close'])
+    TR = []
+    for x in range(len(df_atr)):
+        TR.append(max(a[x],b[x],c[x]))
+    df_atr['TR'] = TR
+    df_atr['ATR'] = df_atr['TR'].rolling(window=14).mean()
+    return df_atr['ATR']
+
+
+df = get_btc_OHLC()
+df['rsi_14']= get_rsi(df,14)
+df['ma_5'],df['ma_10'],df['ma_25'],df['ma_50'],df['ma_100'],df['ma_200'] = get_MA(df)
+df['ema_20'],df['ema_30'] = get_EMA(df)
+df['SMI'] = get_SMI(df)
+df['CCI'] = get_CCI(df)
+df['william_a/d'] = get_william_A_D(df)
+df['ATR'] = get_ATR(df)
+
+df.to_csv('../data/data.csv')
+
+if __name__ == '__main__':
+
